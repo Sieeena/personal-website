a445cec5f5a8c4562fde750ad747031dc2d67128
Sieeena <siena.van.schaick@gmail.com>
2026-08-20 17:52:00 +0200

    change paths for github pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 export default function main(config) {
     config.addPassthroughCopy("src/static")
-  return { dir: { input: "src", output: "pages" } };
+    return { 
+      dir: { input: "src", output: "pages" },
+      pathPrefix: process.env.GITHUB_ACTIONS ? "/my-repo/" : "/",
+    };
 }
